dd2edc07f87fe391c88e6994de7fa88dd8938afa
Jack <edelist@bu.edu>
2022-11-24 23:51:09 -0500

    Initial Commit

---
+++ Gophur/GophurApp.swift
@@ -0,0 +1,17 @@
+//
+//  GophurApp.swift
+//  Gophur
+//
+//  Created by Jack Edelist on 2022-11-24.
+//
+
+import SwiftUI
+
+@main
+struct GophurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
