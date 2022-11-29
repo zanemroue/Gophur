1653430b803b1eacd77a0cffcef0cf20d6d2d9b3
Jack <edelist@bu.edu>
2022-11-28 23:57:14 -0500

    new

--- Gophur/ContentView.swift
+++ Gophur/ContentView.swift
@@ -7,23 +7,31 @@
 
 import SwiftUI
 import Firebase
+import UIKit
 
 struct ContentView: View {
-
+    
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        storyboardview().edgesIgnoringSafeArea(.all)
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    
+    
+    struct storyboardview: UIViewControllerRepresentable {
+        func makeUIViewController(context: Context) -> some UIViewController {
+            let storyboard = UIStoryboard(name: "Home", bundle: Bundle.main)
+            let controller = storyboard.instantiateViewController(identifier: "Home")
+            return controller
+        }
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+            //code
+        }
     }
 }
