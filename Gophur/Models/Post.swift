f88f0e6cee443df96f5fa7e054bbabb709ac3904
hilariog <hilario@bu.edu>
2022-12-11 20:48:28 -0500

    Firebase Managers and User/post model struct

---
+++ Gophur/Models/Post.swift
@@ -0,0 +1,16 @@
+//
+//  Post.swift
+//  Gophur
+//
+//  Created by Hilario Gonzalez on 12/11/22.
+//
+
+import Foundation
+
+struct Post {
+    let identifier: String
+    let title: String
+    let timestamp: TimeInterval
+    let headerImageUrl: URL?
+    let text: String
+}
