5b3462f890e9a7b13bc5a353a59b8212ce77eb24
JoshBard <40773919+JoshBard@users.noreply.github.com>
2022-12-13 00:45:43 -0500

    Update HomeSwiftUIView.swift

--- Gophur/SwiftUI Views/HomeSwiftUIView.swift
+++ Gophur/SwiftUI Views/HomeSwiftUIView.swift
@@ -1,48 +1,95 @@
 //  HomeSwiftUIView.swift
+
 //  Gophur
+
 //Created by Zane Mroue on 11/12/2022.
+
 //
+
 import SwiftUI
 
+
+
 struct HomeSwiftUIView: View {
+
     @State var activeTab:Int = 1//variable for selected tab user makes default is set to first option
+
     @State var selectedDate: Date = Date()
+
     //@ObservedObject var viewModel = PostViewModel()
+
     var body: some View {
+
         TabView(selection: $activeTab) {//tab view for friends, profile and feed page
+
             FriendsView().tabItem { Image(systemName: "person.3.fill") }.tag(1)
+
             FeedView1()
+
                 .tabItem { Image(systemName:
+
                 "list.bullet") }.tag(2)
+
             ProfileView1()
+
                 .tabItem { Image(systemName:
+
                 "person.fill") }.tag(3)
-            /*DatePickerCalendar()
+
+            BusinessProfileHeader()
+
               .tabItem {
+
                 Label("DatePicker Calendar", systemImage: "calendar.badge.plus")
+
                       .padding().tag(4)
-              }*/
-            
-            CalendarView().ignoresSafeArea()
-              .tabItem {
-                Label("Fullscreen Calendar", systemImage: "calendar")
-                      .padding().tag(5)
+
               }
+
+            
+
+            CalendarView().ignoresSafeArea()
+
+              .tabItem {
+
+                Label("Fullscreen Calendar", systemImage: "calendar")
+
+                      .padding().tag(5)
+
+              }
+
             MapView().tabItem { Image(systemName: "map") }.tag(6)
+
         }.onAppear {
+
             let appearance = UITabBarAppearance()
+
             appearance.backgroundColor = UIColor(Color.purple.opacity(0.2))
+
             appearance.shadowColor = UIColor(.purple)
+
             appearance.backgroundEffect = UIBlurEffect(style: .extraLight)
+
             UITabBar.appearance().standardAppearance = appearance
+
             UITabBar.appearance().scrollEdgeAppearance = appearance
+
         }
+
         
+
     }
+
 }
 
+
+
 struct HomeSwiftUIView_Previews: PreviewProvider {
+
     static var previews: some View {
+
         HomeSwiftUIView()
+
     }
+
 }
